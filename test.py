d6f41e045c7c3a3335196ca529bb874c4e77c608
jaclanL <44745949+jaclanL@users.noreply.github.com>
2018-11-15 16:43:49 +0100

    Update test.py
    Removed irrelevant comments

--- test.py
+++ test.py
@@ -1,29 +1,10 @@
-"""
-Write a program that prints the numbers from 1 to 20. But for multiples of three, print 'Fizz'
-instead of the number and for the multiples of five print 'Buzz'.
-For numbers which are multiples of both three and five print 'FizzBuzz'.
-"""
+
 import tensorflow as ts
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 import io
-"""
-for num in range(1,21):
-    string = ""
-    if num % 3 == 0:
-        string = string + "Fizz"
-    if num % 5 == 0:
-        string = string + "Buzz"
-    if num % 5 != 0 and num % 3 != 0:
-        string = string + str(num)
-    print(string)
-
-x = 4
-if x == 4:
-    print("Hello, your number is ",x)
-"""
 
 # Loading data and converting it into a .csv file
 data_xls = pd.read_excel('Sarcos.xls', 'Sheet1', index_col=None)
