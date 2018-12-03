96b11eda41e02fb3fde0f9887cdbb0debf9f3589
Simon Lillskog <37773920+Lillskog@users.noreply.github.com>
2018-12-03 09:12:49 +0100

    Delete normalize_input.m

--- normalize_input.m
+++
@@ -1,6 +0,0 @@
-function normInputSequence = normalize_input(inputSequence)
-iMu = mean(inputSequence, 1);
-[iMax,~] = max(abs(inputSequence),[], 1);
-normInputSequence = (inputSequence-iMu)./iMax;
-end
-
